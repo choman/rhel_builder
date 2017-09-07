aff1c676801a0d43e1e24c040b19241eddb8fe73
Chad Homan <choman@gmail.com>
2017-09-07 11:06:08 -0500

    removed iso variable

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ source version
 echo \$verion = $verion
 echo \$img_name = $img_name
 
-iso=~/Downloads/ISO/rhel-workstation-7.4-x86_64-dvd.iso
+
 
 
 docker container run -v /mnt:/mnt -v `pwd`/output:/output ${img_name}
