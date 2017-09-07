6978cb52c99e7e6f34849c9358411b5e1f63965c
Chad Homan <choman@gmail.com>
2017-09-07 11:24:09 -0500

    removed JEDI code

--- src/script.sh
+++ src/script.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
 RHEL_DIR=/rhel
-JEDI=0
 
 mkdir -p ${RHEL_DIR}/var/lib/rpm
 rpm --root ${RHEL_DIR} --initdb
 
-if [ -f "/mnt/JEDI.tgz" ]; then
-    BASE="/mnt/Packages/rhel-x86_64-server-6/getPackage"
-else
-    BASE="/mnt/Packages"
-fi
+BASE="/mnt/Packages"
 
 rpm --root ${RHEL_DIR} -ihv ${BASE}/redhat-release*
 
@@ -29,10 +24,6 @@ echo "installing software"
 yum -y --installroot=${RHEL_DIR} localinstall ${BASE}/unzip*rpm
 
 echo "RHEL Docker Image" > ${RHEL_DIR}/etc/motd
-
-##echo "Something Something JEDI"
-##tar -C ${RHEL_DIR}/opt -xpf /scripts/JEDI.tgz
-##chroot ${RHEL_DIR} /opt/JEDI/Scripts/config.Lockdown -s /opt/JEDI/config
 
 VERSION=$(awk  '{print $7'} ${RHEL_DIR}/etc/redhat-release)
 TYPE=$(awk -F: '{print $7'} ${RHEL_DIR}/etc/system-release-cpe)
